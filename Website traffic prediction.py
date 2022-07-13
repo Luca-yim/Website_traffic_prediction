8159a131be6926de6949d9a8b1ed780484655667
Luca-yim <82668646+Luca-yim@users.noreply.github.com>
2022-07-13 04:15:18 +0100

    source code of model and data set
    So far, i have imported the needed modules and can read the print the head of my dataset

---
+++ Website traffic prediction.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.graphics.tsaplots import plot_pacf
+from statsmodels.tsa.arima_model import ARIMA
+import statsmodels.api as sm
+
+data = pd.read_csv(r"C:\Users\guest\Thecleverprogrammer.csv")
+print(data.head())
