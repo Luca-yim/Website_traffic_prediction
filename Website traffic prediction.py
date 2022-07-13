f7cbab9cd6d6c777f89efae73c9e4abc26e436b0
unknown <Luca-yim>
2022-07-13 05:02:51 +0100

    Modified python code
    Changed date field to dd/mm/yy format and can now plot date vs traffic

--- Website traffic prediction.py
+++ Website traffic prediction.py
@@ -7,5 +7,18 @@ from statsmodels.graphics.tsaplots import plot_pacf
 from statsmodels.tsa.arima_model import ARIMA
 import statsmodels.api as sm
 
+#Reading data set
 data = pd.read_csv(r"C:\Users\guest\Thecleverprogrammer.csv")
-print(data.head())
+#print(data.head())
+
+#Changing date field to dd/mm/yy format
+data["Date"] = pd.to_datetime(data["Date"], format = "%d/%m/%Y")
+
+#print(data.info())
+
+#Plot of daily traffic
+plt.style.use('fivethirtyeight')
+plt.figure(figsize= (15, 10))
+plt.plot(data["Date"], data["Views"])
+plt.title(" Daily Traffic of Thecleverprogrammer.com ") 
+plt.show()
